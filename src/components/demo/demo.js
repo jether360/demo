b048edeebc39e71952fee502cb642e7d15a6454e
unknown <jether.balabagno@aquilasoftwares.com>
2022-03-01 20:29:08 +0800

    create demo

---
+++ src/components/demo/demo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Demo = () => {
+    return (
+        <div>
+        <h1>Hello Bojan</h1>
+        <h1>Hello Regime</h1>
+        </div>
+    );
+}
+
+export default Demo;
